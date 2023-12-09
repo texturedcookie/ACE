8b92897b67ce94ae9a29cdec4258ea903ad23379
texturedcookie <78194195+texturedcookie@users.noreply.github.com>
2023-12-09 08:37:45 -0600

    Login screen functionality and added user to context screen

--- Ace/Context/AceContext.js
+++ Ace/Context/AceContext.js
@@ -4,10 +4,26 @@ const AceContext = createContext();
 
 export const AceProvider = ({ children }) => {
    
-    const aceContextValue = {
-      
-    "NAME":"hi"
-    };
+  const [users, setUsers] = useState([
+   {"id":"1",
+    "name":"John Doe",
+    "email":"john@example.com",
+    "password":"password",
+    "role":"admin",
+    "studyPlans": [{"subject":"Bio",
+                    "learningStyle":"visual",
+                    " setsofstudyplan":[
+
+                      {"setname":"set1",
+                      "setdescription":"set1 description",
+                      "settype":"colorFlashCards"},
+                      ]}]
+  },
+
+ ]);
+
+
+    const aceContextValue = { users};
   
     return (
       <AceContext.Provider value={aceContextValue}>
---
+++ Ace/Screens/LoginScreen.js
@@ -0,0 +1,66 @@
+import React, { useContext, useState } from "react";
+import { View, Text, TextInput, Button, StyleSheet } from "react-native";
+import AceContext, { AceProvider } from "../Context/AceContext";
+
+
+
+const LoginScreen = ({ navigation }) => {
+  const { users } = useContext(AceContext);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = () => {
+    // Check if the provided email and password match any user
+    const user = users.find(
+      (user) => user.email === email && user.password === password
+    );
+
+    if (user) {
+      // Navigate to the next screen (you can replace 'Home' with your desired screen)
+      navigation.navigate("Home");
+    } else {
+      // Display an error message or handle the invalid login
+      alert("Invalid email or password");
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text>Email:</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Enter your email"
+        onChangeText={(text) => setEmail(text)}
+        value={email}
+      />
+      <Text>Password:</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Enter your password"
+        secureTextEntry
+        onChangeText={(text) => setPassword(text)}
+        value={password}
+      />
+      <Button title="Login" onPress={handleLogin} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  input: {
+    width: 200,
+    height: 40,
+    borderColor: "gray",
+    borderWidth: 1,
+    marginBottom: 20,
+    padding: 10,
+  },
+});
+
+export default LoginScreen;
+
